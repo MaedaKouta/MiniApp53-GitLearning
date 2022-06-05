b9e6018e90b1c50966c004502438b938348b5657
Kota <maedakoutaa@gmail.com>
2022-06-05 15:18:31 +0900

    aaa

---
+++ MiniApp53-GitLearning/aaaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  aaaViewController.swift
+//  MiniApp53-GitLearning
+//
+//  Created by 前田航汰 on 2022/06/05.
+//
+
+import UIKit
+
+class aaaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
